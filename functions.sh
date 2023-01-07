f42fca239fc65187f3ace2f03542a1088c6e5460
Mikhail Tsay <37892513+MkTsNix@users.noreply.github.com>
2023-01-07 21:51:33 +0600

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
 # Put your code here
+
+pow() {
+    echo $(( $1 ** $2 ))
+}
+
+shortest() {
+  min=()
+  for i in "$@"; do
+    if [ ${#min} -eq 0 ]; then
+      min+=("$i")
+    elif [[ ${#i} -lt "${#min[0]}" ]]; then
+      min=("$i")
+    elif [[ ${#i} -eq "${#min[0]}" ]]; then
+      min+=("$i")
+    fi
+  done
+  echo "${min[@]}" | xargs -n 1
+}
+
+print_log() {
+    param_msg=${*}
+    echo "[$(date +%F" "%R)] $param_msg"
+}
